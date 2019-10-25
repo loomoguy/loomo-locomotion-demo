4ab9403869c42ec5957d906ff6695c650db49592
Marshall <marshrich3@gmail.com>
2019-10-25 13:43:46 -0500

    initial commit

---
+++ app/src/main/java/com/example/loomo_demo/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.loomo_demo;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.segway.robot.algo.Pose2D;
+import com.segway.robot.algo.minicontroller.CheckPoint;
+import com.segway.robot.algo.minicontroller.CheckPointStateListener;
+import com.segway.robot.sdk.base.bind.ServiceBinder;
+import com.segway.robot.sdk.locomotion.sbv.Base;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    Base mBase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Loomo Implementation
+        mBase = Base.getInstance();
+        mBase.bindService(getApplicationContext(), new ServiceBinder.BindStateListener() {
+            @Override
+            public void onBind() {
+                mBase.setOnCheckPointArrivedListener(new CheckPointStateListener() {
+                    @Override
+                    public void onCheckPointArrived(CheckPoint checkPoint, Pose2D realPose, boolean isLast) {
+
+                    }
+
+                    @Override
+                    public void onCheckPointMiss(CheckPoint checkPoint, Pose2D realPose, boolean isLast, int reason) {
+
+                    }
+                });
+
+
+            }
+
+            @Override
+            public void onUnbind(String reason) {
+
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                mBase.setControlMode(Base.CONTROL_MODE_RAW);
+//                mBase.setLinearVelocity(1f);
+
+                mBase.setControlMode(Base.CONTROL_MODE_NAVIGATION);
+                mBase.cleanOriginalPoint();
+                Pose2D pose2D = mBase.getOdometryPose(-1);
+                mBase.setOriginalPoint(pose2D);
+                mBase.addCheckPoint(1f, 0f);
+                mBase.addCheckPoint(1f, 1f);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
